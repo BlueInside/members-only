8b633c833ef3c2dca1bc43f11d4f3c7b466ac65c
Karol <kpulawski13@gmail.com>
2024-03-19 18:04:06 +0000

    connect to database

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Connect to the database
+require('./config/database');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
