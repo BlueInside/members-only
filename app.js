ddd9beaf79d86ef8ba98006906f2e09e1c7cf8e5
Karol <kpulawski13@gmail.com>
2024-03-20 14:10:50 +0000

    Set up session middleware in app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+// Gives access to the environmental variable
+require('dotenv').config();
 
 // Connect to the database
 require('./config/database');
@@ -15,12 +21,30 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Express session setup
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL,
+      dbName: 'members_only',
+    }),
+  })
+);
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Passport authentication
+// require('./config/passport');
+
+// Routes handler
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
