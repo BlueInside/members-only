140c639ea9b9bcce40acb6e489faee04b143a84f
Karol <kpulawski13@gmail.com>
2024-03-20 10:26:32 +0000

    Add memberStatus path for user schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String, required: true, minLength: 1 },
   userName: { type: String, required: true, minLength: 1, unique: true }, // Required username which will be email
   password: { type: String, required: true }, // Hashed password
+  memberStatus: { type: Boolean, required: true },
 });
 
 userSchema.virtual('fullName').get(function () {
