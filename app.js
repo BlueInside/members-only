6aca0f408e3b37b69a19e1d4d11968e06b84a823
Karol <kpulawski13@gmail.com>
2024-03-24 11:16:35 +0000

    Add rate limiter

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Set up rate limiter
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 40,
+});
 // Express session setup
 app.use(
   session({
@@ -35,6 +41,7 @@ app.use(
   })
 );
 
+app.use(limiter);
 app.use(helmet());
 app.use(passport.session());
 app.use(logger('dev'));
